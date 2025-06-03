add0f27db964f77b4ad6bb2cbf389dbab747df7e
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-06-03 17:32:25 +0530

    upadte favicon

--- resources/views/layouts/main-app.blade.php
+++ resources/views/layouts/main-app.blade.php
@@ -31,7 +31,7 @@
     <!-- MONO CSS -->
     <link id="main-css-href" rel="stylesheet" href="{{ asset('assets/theme/css/style.css') }}" />
     <!-- FAVICON -->
-    <link href="{{ asset('assets/theme/images/favicon.png') }}" rel="shortcut icon" />
+    <link href="{{ asset('storage/favicons/favicon.png') }}" rel="favicon icon" />
     <!--
     HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
   -->
