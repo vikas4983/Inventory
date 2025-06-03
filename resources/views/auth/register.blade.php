31c97ec4ae3b6e684d60ef74c88cb999403eaecc
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-06-03 20:09:57 +0530

    Authentication

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Mono - Responsive Admin & Dashboard Template</title>
+        <title>Inventory - Register</title>
 
         <!-- GOOGLE FONTS -->
         <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
