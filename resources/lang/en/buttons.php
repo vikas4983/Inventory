7bdcf8a5b3e3efdd2a6f8032cc9378fa67cf4765
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-06-12 13:08:35 +0530

    update Buttons

--- resources/lang/en/buttons.php
+++ resources/lang/en/buttons.php
@@ -1,19 +1,11 @@
-<?php 
+<?php
 return [
     'create' => 'Create',
-<<<<<<< HEAD
-=======
-    'add_new' => 'Add New Category',
-    'add_new_brand' => 'Add New Brand',
->>>>>>> 380ecaa4523a84112fff0746f8f8ab6de21df1b9
     'update' => 'Update',
     'delete' => 'Delete',
     'cancel' => 'Cancel',
-      'add_new' => 'Add New Category',
-      'add_new_product' => 'Add New Product',
+    'add_new' => 'Add New Category',
+    'add_new_brand' => 'Add New Brand',
+    'add_new_product' => 'Add New Product',
 
 ];
-
-
-
-?>
